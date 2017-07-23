0bb85455017cab49da7e14e8cb79af9923639d7a
kpkalmus <piotrek-kalmus@web.de>
2017-07-23 11:04:39 +0200

    Added alive cells by click.

--- src/com/tarosso/game/of/life/Main.java
+++ src/com/tarosso/game/of/life/Main.java
@@ -17,17 +17,78 @@ public class Main {
 	public static final int FRAME_WIDTH = 800;
 	public static final int FRAME_HEIGHT = 600;
 	
-	public static final int GAMEFIELD_SIZE = 50;
-	public static final int MAX_ALIVE_BY_START = 500;
+	public static final int GAMEFIELD_SIZE = 70;
+	public static final int MAX_ALIVE_BY_START = 1000;
 	
 	public static void main(String[] args) {
 		Cell.rules = new ArrayList<>();
 		Cell[][] gamefield = new Builder.GamefieldBuilder(GAMEFIELD_SIZE)
-										.aliveCell(1,0)
-										.aliveCell(2, 1)
-										.aliveCell(0, 2)
-										.aliveCell(1, 2)
-										.aliveCell(2, 2)
+//										.aliveCell(2,1)
+//										.aliveCell(3, 2)
+//										.aliveCell(1, 3)
+//										.aliveCell(2, 3)
+//										.aliveCell(3, 3)
+//										.aliveCell(47, 1)
+//										.aliveCell(46, 2)
+//										.aliveCell(48, 3)
+//										.aliveCell(47, 3)
+//										.aliveCell(46, 3)
+//										.aliveCell(2, 48)
+//										.aliveCell(3, 47)
+//										.aliveCell(1, 46)
+//										.aliveCell(2, 46)
+//										.aliveCell(3, 46)
+//										.aliveCell(47, 48)
+//										.aliveCell(46, 47)
+//										.aliveCell(48, 46)
+//										.aliveCell(47, 46)
+//										.aliveCell(46, 46)
+				
+				
+//										// Rectangle Left
+//										.aliveCell(0, 4)
+//										.aliveCell(0, 5)
+//										.aliveCell(1, 4)
+//										.aliveCell(1, 5)
+//										// Rectangle Right
+//										.aliveCell(34, 2)
+//										.aliveCell(34, 3)
+//										.aliveCell(35, 2)
+//										.aliveCell(35, 3)
+//										//Rectangle Middle
+//										.aliveCell(16, 4)
+//										.aliveCell(16, 5)
+//										.aliveCell(16, 6)
+//										.aliveCell(17, 4)
+//										.aliveCell(17, 5)
+//										.aliveCell(17, 6)
+//										//object left
+//										.aliveCell(12, 2)
+//										.aliveCell(12, 3)
+//										.aliveCell(11, 3)
+//										.aliveCell(11, 4)
+//										.aliveCell(10, 4)
+//										.aliveCell(11, 5)
+//										.aliveCell(10, 5)
+//										.aliveCell(9, 5)
+//										.aliveCell(11, 6)
+//										.aliveCell(10, 6)
+//										.aliveCell(12, 7)
+//										.aliveCell(11, 7)
+//										.aliveCell(12, 8)
+//										//object rigth
+//										.aliveCell(23, 0)
+//										.aliveCell(23, 1)
+//										.aliveCell(21, 1)
+//										.aliveCell(22, 2)
+//										.aliveCell(20, 2)
+//										.aliveCell(22, 3)
+//										.aliveCell(19, 3)
+//										.aliveCell(22, 4)
+//										.aliveCell(20, 4)
+//										.aliveCell(23, 5)
+//										.aliveCell(21, 5)
+//										.aliveCell(23, 6)
 										.build();
 		Cell.rules.add(new RuleThree());
 		Cell.rules.add(new RuleOne());
@@ -42,7 +103,7 @@ public class Main {
 			frame.updateCells();
 			checkRules(gamefield);
 			try {
-				Thread.sleep(250);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				break;
 			}
@@ -54,6 +115,7 @@ public class Main {
 			for(int x = 0; x < gamefield.length; x++) {
 				frame.appendCellPanel(new CellPanel(gamefield[x][y]));
 			}
+			frame.updateCells();
 		}
 	}
 	
--- src/com/tarosso/game/of/life/gui/CellPanel.java
+++ src/com/tarosso/game/of/life/gui/CellPanel.java
@@ -1,6 +1,8 @@
 package com.tarosso.game.of.life.gui;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
@@ -14,7 +16,9 @@ public class CellPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public static final int SIZE = 20;
+	public static final int SIZE = 15;
+
+	public static boolean mousePressed;
 	
 	private Cell cell;
 	
@@ -24,11 +28,52 @@ public class CellPanel extends JPanel {
 		setBackground(cell.isAliveColor());
 		setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		setBounds(SIZE * cell.getPosition().getPosX(), SIZE * cell.getPosition().getPosY()-1, SIZE, SIZE);
+		addMouseListener(new CellAliveListener());
 		setVisible(true);
 	}
 	
 	public void updateColor() {
 		setBackground(this.cell.isAliveColor());
+	}
+	
+	public class CellAliveListener implements MouseListener {
+		
+		public CellAliveListener() {
+			mousePressed = false;
+		}
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			mousePressed = true;
+			if(cell.isAlive()) {
+				cell.die();
+			} else {
+				cell.alive();
+			}
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			mousePressed = false;
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			if(mousePressed) {
+				if(cell.isAlive()) {
+					cell.die();
+				} else {
+					cell.alive();
+				}
+			}
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
+		
 	}
 	
 }
